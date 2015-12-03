61bac53fd452318195bf8268565ad74276435d74
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-03 23:38:30 +0100

    Change default responses

--- api/responses/badRequest.js
+++ api/responses/badRequest.js
@@ -15,12 +15,14 @@
  * ```
  */
 
-module.exports = function badRequest(data, options) {
+module.exports = function badRequest(data, details) {
 
   // Get access to `req`, `res`, & `sails`
   var req = this.req;
   var res = this.res;
   var sails = req._sails;
+
+  details = details || null;
 
   // Set status code
   res.status(400);
@@ -34,31 +36,10 @@ module.exports = function badRequest(data, options) {
   // Only include errors in response if application environment
   // is not set to 'production'.  In production, we shouldn't
   // send back any identifying information about errors.
-  if (sails.config.environment === 'production') {
+  console.log(data, typeof data);
+  if (sails.config.environment === 'production' && typeof data !== 'string') {
     data = undefined;
   }
 
-  // If the user-agent wants JSON, always respond with JSON
-  if (req.wantsJSON) {
-    return res.jsonx(data);
-  }
-
-  // If second argument is a string, we take that to mean it refers to a view.
-  // If it was omitted, use an empty object (`{}`)
-  options = (typeof options === 'string') ? { view: options } : options || {};
-
-  // If a view was provided in options, serve it.
-  // Otherwise try to guess an appropriate view, or if that doesn't
-  // work, just send JSON.
-  if (options.view) {
-    return res.view(options.view, { data: data });
-  }
-
-  // If no second argument provided, try to serve the implied view,
-  // but fall back to sending JSON(P) if no view can be inferred.
-  else return res.guessView({ data: data }, function couldNotGuessView () {
-    return res.jsonx(data);
-  });
-
+  return res.json({error: data, details: details});
 };
-
