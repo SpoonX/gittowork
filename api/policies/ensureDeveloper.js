0dac375961e71ebec998d3ff03e4f304f3a2c925
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-05 15:01:15 +0100

    Added ensureDeveloper policy

---
+++ api/policies/ensureDeveloper.js
@@ -0,0 +1,28 @@
+var helpers = require('request-helpers');
+
+/**
+ * Policy which makes sure the user exists, and otherwise creates it.
+ */
+module.exports = function (req, res, next) {
+  var params = helpers.secureParameters(['username'], req);
+
+  if (!params.isValid()) {
+    return res.badRequest('missing_parameter', 'username');
+  }
+
+  var username = params.get('username');
+
+  Developer.count({username: username}).then(function (count) {
+    if (count > 0) {
+      return next();
+    }
+
+    GithubService.importDeveloper(username, function (error) {
+      if (error) {
+        return res.serverError(error);
+      }
+
+      next();
+    });
+  }).catch(res.serverError);
+};
