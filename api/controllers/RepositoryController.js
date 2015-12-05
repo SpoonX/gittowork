14a0f450a7dfb06f7b6c60a4a20001f6b3542410
RWOverdijk <r.w.overdijk@gmail.com>
2015-12-05 16:05:56 +0100

    Repository controller

---
+++ api/controllers/RepositoryController.js
@@ -0,0 +1,11 @@
+/**
+ * RepositoryController
+ *
+ * @description :: Server-side logic for managing repositories
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
